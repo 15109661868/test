b5292944e735a7fa96a95017bdfc44495dae0bbc
guo-meng-chen <2209310973@qq.com>
2022-04-15 22:15:10 +0800

    练习‘

---
+++ 4.15/4.15/if语句.4.15.c
@@ -0,0 +1,100 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h>
+//int main(﻿)
+//{
+//    int a, b, c;
+//    scanf("%d %d %d", &a, &b, &c);
+//    if (a < b)
+//    {
+//        int tmp = a;
+//        a = b;
+//        b = tmp;
+//    }
+//    if (a < c)
+//    {
+//        int tmp = a;
+//        a = c;
+//        c = tmp;
+//    }
+//    if (b < c)
+//    {
+//        int tmp = b;
+//        b = c;
+//        c = tmp;
+//    }
+//    printf("a=%d b=%d c=%d\n", a, b, c);
+//        return 0;
+//}
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int a = 1;
+//	scanf("%d", &a);
+//	while (a <= 100)
+//	{
+//		if (a % 3 == 0)
+//			printf("%d ", a);
+//		a++;
+//	}
+//	return 0;
+//}
+//#include <stdio.h>
+//int main()
+//{
+//	int a,b,c;
+//	scanf("%d %d", &a, &b);
+//	while(c=a%b)
+//	{
+//		a = b;
+//		b = c;
+//	}
+//	printf("%d", b);
+//	return 0;
+//}
+//#include <stdio.h>
+//int main()
+//{
+//	int a = 0;
+//	int r = 1;
+//	int s = 0;
+//	int n = 0;
+//	for (n = 1; n <= 10; n++)
+//	{
+//		int r = 0;
+//		for (a = 1; a <= n; a++)
+//		{
+//			r = r * a;
+//		}
+//		s = s + r;
+//	}
+//	printf("%d", &s);
+//	return 0;
+//}
+//#include <stdio.h>
+//int main()
+//{
+//	long ma = 0;
+//	scanf("%d", &ma);
+//	for (ma = 1000; ma <= 2000; ma++)
+//		if (ma % 4 == 0)
+//		{
+//			printf("%d  ", ma);
+//		}
+//
+//	return 0;
+//}
+//#include <stdio.h>
+//
+//int main()
+//{
+//	long a = 0;
+//	for (a = 100; a <= 200; a++)
+//	{
+//		if (a % 2 != 0)
+//		{
+//			printf("%d  ", a);
+//		}
+//	}
+//	return 0;
+//}
