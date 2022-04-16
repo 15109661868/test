0395eb5afa04cb453e0bb0cde6f3b1267288192d
guo-meng-chen <2209310973@qq.com>
2022-04-16 22:15:23 +0800

    练习14

---
+++ 4.16/4.16/4.16.c
@@ -0,0 +1,49 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h>
+//int main()
+//{
+//	int math = 0;
+//	for (math = 1; math <= 100; math++)
+//	{
+//		if (math % 10 == 9)
+//		{
+//			printf("%d  ", math);
+//		}
+//	}
+//	return 0;
+//}
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int a = 0;
+//	for (a = 1; a <= 9; a++)
+//	{
+//		int b = 1;
+//		for(b=1;b<=a;b++)
+//		{ 
+//			printf("%d*%d=%2d ",b,a,a*b);
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int r = 0;
+//	scanf("%d", &r);
+//	printf("%.2f", r*r * 3.14);
+//	return 0;
+//}
+#include <stdio.h>
+
+int main()
+{
+	int a = 0;
+	int b = 0;
+	scanf("%d %d", &a, &b);
+	printf("%f", a /1+ b/1);
+	return 0;
+}
