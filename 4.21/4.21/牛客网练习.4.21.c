a494d5f6dac1cdcb035230f8adada12027f01f6d
guo-meng-chen <2209310973@qq.com>
2022-04-21 22:24:50 +0800

    练习

---
+++ 4.21/4.21/牛客网练习.4.21.c
@@ -0,0 +1,60 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h>
+//
+//int main()
+//{
+//	long yers = 0;
+//	int mat = 0;
+//	int day = 0;
+//	scanf("%d %d %d", &yers, &mat, &day);
+//
+//	return 0;
+//}
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int a = 1;
+//	int r = 0;
+//	int s = 1;
+//	for (a = 1; a <= 10; a++)
+//	{
+//		s = s * a;
+//		r = r + s;
+//	}
+//	printf("%d", r);
+//		return 0;
+//}
+#include <stdio.h>
+
+int main()
+{
+	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+	int k = 7;
+	
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	int left = 0;
+	int right = sz-1;	
+	while (left <= right)
+	{
+		int mi = (left + right) / 2;
+		if (arr[mi] < k)
+		{
+			left = mi + 1;
+		}
+		else if (arr[mi] > k)
+		{
+			right = mi - 1;
+		}
+		else
+		{
+			printf("找到了,下标是:%d\n", mi);
+			break;
+		}
+	}
+	if (left > right)
+	{
+		printf("找不到\n");
+	}
+	return 0;
+}
