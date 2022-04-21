3f4e2f58a3fdd4299be7d074a501984fb136bae0
guo-meng-chen <2209310973@qq.com>
2022-04-21 20:46:50 +0800

    练习

---
+++ Project1/Project1/4.20.c
@@ -0,0 +1,123 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h>
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	int c = 0;
+//	scanf("%d %d %d", &a, &b,&c);
+//
+//	return 0;
+//}
+//
+//#include <stdio.h>
+//
+//
+//int main()
+//{
+//	int a = 0;
+//	double su = 0.0;
+//	int fl = 1;
+//	for (a = 1; a <= 100; a++)
+//	{
+//		su += fl *1.0/ a;
+//		fl = -fl;
+//	}
+//	printf("%f", su);
+//	return 0;
+//}
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int arr[ 10 ] = { 0 };
+//	int a = 0;
+//	int max = 0;
+//	for (a = 0; a < 10; a++)
+//	{
+//		scanf("%d", &arr);
+//	}
+//	max = arr[0];
+//	for (a = 0; a < 10; a++)
+//	{
+//		if (arr[a] > max)
+//		{
+//			max = arr[a];
+//		}
+//	}
+//	printf("%d", max);
+//	return 0;
+//}
+//#include <stdlib.h>
+//#include <time.h>
+//
+//
+//void menu()
+//{
+//	printf("********************************\n");
+//	printf("*******     1. play      *******\n");
+//	printf("*******     0. exit      *******\n");
+//	printf("********************************\n");
+//}
+//
+//
+//void game()
+//{
+//	int ret = rand() % 100 + 1;
+//	int num = 0;
+//	while (1)
+//	{
+//		printf("请猜数字:>");
+//		scanf("%d", &num);
+//		if (num == ret)
+//		{
+//			printf("恭喜你，猜对了\n");
+//			break;
+//		}
+//		else if (num > ret)
+//		{
+//			printf("猜大了\n");
+//		}
+//		else
+//		{
+//			printf("猜小了\n");
+//		}
+//	}
+//}
+//
+//
+//int main()
+//{
+//	int input = 0;
+//	srand((unsigned int)time(NULL));
+//	do
+//	{
+//		menu();
+//		printf("请选择:>");
+//		scanf("%d", &input);
+//		switch (input)
+//		{
+//		case 1:
+//			game();
+//			break;
+//		case 0:
+//			printf("退出游戏\n");
+//			break;
+//		default:
+//			printf("选择错误\n");
+//			break;
+//		}
+//	} while (input);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	int c = 0;
+//	for(a=)
+//	return 0;
+//}
