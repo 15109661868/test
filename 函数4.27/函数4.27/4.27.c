1cfa92101ce758cefd6aa52c18b0d405749ee937
guo-meng-chen <2209310973@qq.com>
2022-06-21 11:39:57 +0800

    练习

---
+++ 函数4.27/函数4.27/4.27.c
@@ -0,0 +1,64 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+//#include <stdio.h>
+//
+//void swap(int *py,int*pn)
+//{
+//	int v = 0;
+//	v = *py;
+//	*py = *pn;
+//	*pn = v;
+//}
+//int Add(int x, int y)
+//{
+//	int v = 0;
+//	v = x + y;
+//	return v;
+//}
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	scanf("%d%d", &a,&b);
+//	int c=Add(a, b);
+//	printf("计算后%d\n",c);
+//	printf("输入前%d %d\n",a,b);
+//	swap(&a,&b);
+//	printf("输入后%d %d\n", a, b);
+//	return;
+//}
+#include <stdio.h>
+
+//
+//int main()
+//{
+//	int l = 0;
+//	int r = 0;
+//	int i = 0;
+//	double sum = 0.0;
+//	scanf("%d", &l);
+//	scanf("%d %d", &r, &i);
+//	for (l = 1; l <= 100; l++)
+//	{
+//		for (r = 1; r <= l; r++)
+//		{
+//			if (r > 60)
+//			{
+//				sum = sum + 0.2;
+//			}
+//			else
+//			{
+//				sum = sum + 0.1;
+//			}
+//		}
+//	}
+//	printf("%.1ld", sum);
+//	return 0;
+//}
+#include <stdio.h>
+
+int main()
+{
+
+	return 0;
+}
