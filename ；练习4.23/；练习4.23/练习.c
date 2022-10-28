ab511764d2a6c3c13962e001e6e070a736903160
guo <2209310973@qq.com>
2022-10-28 17:57:13 +0800

    练习

---
+++ ；练习4.23/；练习4.23/练习.c
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h>
+////
+//int main()
+//{
+//	int h = 0;
+//	int r = 0;
+//	double ma = 0;
+//	scanf("%d %d", &h, &r);
+//	ma = (3.14 * h * r * r)/1000;
+//	printf("%d", 10 / ma);
+//	return 0;
+//}*/
+#include <stdio,h>
+int main()
+{
+	int a = 0;
+	scanf("%d", &a);
+
+	return 0;
+}
