9f1ae606387a345dfd9a4412800ab00ef2410182
guo <2209310973@qq.com>
2022-10-28 17:57:53 +0800

    练习

---
+++ 4.18/4.18/4.17.c
@@ -0,0 +1,72 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h>
+
+//int main()
+//{
+//	int n = 0;
+//	int h = 0;
+//	int m = 0;
+//	int c = n - m / h;
+//	scanf("%d %d %d", &n, &h, &m);
+//	if (m <= n * h)
+//	{
+//		if (m % h > 0)
+//		{
+//			printf("%d", m / h + 1);
+//		}
+//		else
+//			printf("%d", c);
+//	}
+//	return 0;
+//}
+////#include <stdio.h>
+//int main()
+//{
+//	int h = 0;
+//	int r = 0;
+//	int s = 3.14 * h * r * r;
+//	scanf("%d %d", &h, &r);
+//	printf("%d", s);
+//	return 0;
+//}
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int r = 0;
+//	scanf("%d", &r);
+//	float s = (4.0 / 3.0) * 3.14 * r*r*r;
+//	printf("%f", s);
+//	return 0;
+//}
+//#include <stdio.h>
+//
+//int main()
+//{
+//	long n = 0;
+//	scanf("%d", &n);
+//	if (((n % 4 == 0) && (n % 100 != 0)) || (n % 400 == 0))
+//	{
+//		printf("yes");
+//	}
+//	else
+//		printf("no");
+//
+//
+//	return 0;
+//}
+#include <stdio.h>
+int main()
+{
+    char a ;
+    scanf("%c", &a);
+    if ("a >= a && a <= z || a>=A && a<=Z")
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+    return 0;
+}
