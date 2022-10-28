ffe240f3442ccef91187f16efed956e55da41810
guo <2209310973@qq.com>
2022-10-28 17:58:40 +0800

    练习

---
+++ 5.28/5.28/5.28.c
@@ -0,0 +1,82 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+//int main()
+//{
+//    char str[] = "hello bit";
+//    printf("%d %d\n", sizeof(str), strlen(str));
+//    return 0;
+//}
+//#include <stdio.h>
+//void vernchar(int m[], int n[],int sz)
+//{
+//	int i = 0;
+//	for (i = 0; i < sz; i++)
+//	{
+//		int imp = 0;
+//		imp = m[i];
+//		m[i] = n[i];
+//		n[i] = imp;
+//	}
+//}
+//void print_arr(int a[], int sz)
+//{
+//	int i = 0;
+//	for (i = 0; i < sz; i++)
+//	{
+//		printf("%d", a[i]);
+//	}
+//	printf("\n");
+//}
+//int main()
+/*{
+	*//*int a[3] = {1,2,3 };*/
+	/*int b[3] = {4,5,6};*/
+	/*int sz=sizeof(a)/sizeof(a[0]);*/
+	/*print_arr(a, sz);*/
+	/*print_arr(b, sz);*/
+	/*vernchar(a, b, sz);*/
+	/*printf("½»»»ºó>\n");
+	print_arr(a, sz);
+	print_arr(b, sz);*/
+	/*return 0;
+}*/
+//#include <stdio.h>
+//void init(int a[], int sz)
+//{
+//	int i = 0;
+//	for (i = 0; i < sz; ++i)
+//	{
+//		a[i] = i;
+//	}
+//}
+//void print_arr(int a[],int sz)
+//{
+//	int i = 0;
+//	for(i=0;i<sz;i++)
+//	{
+//		printf("%d", a[i]);
+//    }
+//	printf("\n");
+//}
+//void reverse(int a[], int sz)
+//{
+//	int left = 0;
+//	int right = sz - 1;
+//	while (left<right)
+//	{
+//		int tomp = left;
+//		left = right;
+//		right = tomp;
+//		left++;
+//		right--;
+//	}
+//}
+//int main()
+//{
+//	int a[] = {1,2,3,4,5,6,7};
+//	int sz = sizeof(a) / sizeof(a[0]);
+//	init(a, sz);
+//	print_arr(a, sz);
+//	reverse(a, sz);
+//	return 0;
+//}
